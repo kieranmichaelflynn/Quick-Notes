1e2be32970f03c4b842c80fbd4f25b3ec18e86e6
kieranmichaelflynn <kieranmichaelflynn@gmail.com>
2022-05-03 10:35:20 +0800

    Starter code plus server files

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const webRouter = require('./routes/web');
+const apiRouter = require('./routes/api');
+
+const app = express();
+
+const PORT = 3001;
+app.use( express.static('public'));
+
+app.use(express.json());
+
+app.use(webRouter);
+app.use(apiRouter);
+
+
+app.listen(PORT, function(){
+  console.log(`App is running on http://localhost:${PORT}`);
+});
+
