35662b51710155c649ba6d4423d5a26ee72a619d
kieranmichaelflynn <kieranmichaelflynn@gmail.com>
2022-05-07 18:36:41 +0800

    update7may

--- server.js
+++ server.js
@@ -5,9 +5,10 @@ const apiRouter = require('./routes/api');
 const app = express();
 
 const PORT = process.env.PORT || 3001;
-app.use( express.static('public'));
-
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
 
 app.use("/", webRouter);
 app.use("/api", apiRouter);
